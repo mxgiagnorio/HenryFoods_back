69c5fffce1dee5b442bddff9e4a944ca498402ea
Maxi Giagnorio <maxigiagnorio@gmail.com>
2023-01-30 17:10:49 -0300

    hwrhwhr

--- index.js
+++ index.js
@@ -19,11 +19,11 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const { PORT } = process.env;
+const port = process.env.PORT || 3001;
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
-  server.listen(PORT, () => {
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
+conn.sync({ force: true }).then(() => {
+  server.listen(port, () => {
+    console.log(`Server raised in port ${port}`);
   });
 });
